86ac4c473a2ed158317a2721eaffdda064beba3d
Marco Metz <marco.metz@gmail.com>
2015-04-12 13:54:08 +0200

    production css

--- app/assets/javascripts/foods.js
+++ app/assets/javascripts/foods.js
@@ -1,4 +1,4 @@
 jQuery(document).ready(function($) {
-  $( "#food_embedded_at" ).dateDropper({format: "d.m.Y", lang: "de", minYear: "current year", maxYear: "2025", years_multiple: 5});
-  $( "#food_best_before" ).dateDropper({format: "d.m.Y", lang: "de", minYear: "current year", maxYear: "2025", years_multiple: 5});
+  $( "#food_embedded_at" ).dateDropper({format: "d.m.Y", lang: "de", minYear: "2013", maxYear: "2025", years_multiple: 5});
+  $( "#food_best_before" ).dateDropper({format: "d.m.Y", lang: "de", minYear: "2013", maxYear: "2025", years_multiple: 5});
 });
