507f2137eb5ef1e747cfb611d5f0751ea9186d6f
Marco Metz <marco.metz@gmail.com>
2015-04-12 13:34:04 +0200

    production css

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -15,7 +15,7 @@ class FoodsController < ApplicationController
     @all_qr_code = RQRCode::QRCode.new( "#{@url_for_qr_code_base}", :size => 5, :level => :h )
     @locations = {}
     Food::LOCATIONS.each do |key,value|
-      @locations[key] = RQRCode::QRCode.new( "#{@url_for_qr_code_base}/location/#{key}", :size => 5, :level => :h )
+      @locations[key] = RQRCode::QRCode.new( "#{@url_for_qr_code_base}/location/#{key}", :size => 6, :level => :h )
     end
   end
 
